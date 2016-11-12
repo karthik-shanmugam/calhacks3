eefd6cc7c7e59db1c1455610322fbfb2f9fda87b
karthik-shanmugam <karthik@karthiks.me>
2016-11-12 02:40:14 -0800

    pipeline to imgur

---
+++ face-demo/main.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    Webcam.attach( '#my_camera' );
+
+    function take_snapshot() {
+        Webcam.snap( function(data_uri) {
+            document.getElementById('my_result').innerHTML = '<img src="'+data_uri+'"/>';
+            $("emotion-img").attr("src", "");
+            post("/image", {'image': data_uri}, function(data){
+                // document.getElementById('emotion').innerHTML = data;
+                switch (data) {
+                    case "happines":
+                        $(".emotion-img").attr("src", "emojis/smile.png");
+                        break;
+                    case "sadness":
+                        $(".emotion-img").attr("src", "emojis/frown.png");
+                        break;
+                    default:
+                        $(".emotion-img").attr("src", "emojis/neutral.png");
+                }       
+                console.log(data);
+            });
+        });
+    }
+
+    $(".snap").click(take_snapshot);
+
+    function post(path, parameters, callback) {
+        var form = $('<form></form>');
+
+        form.attr("method", "post");
+        form.attr("action", path);
+
+        $.each(parameters, function(key, value) {
+            var field = $('<input></input>');
+
+            field.attr("type", "hidden");
+            field.attr("name", key);
+            field.attr("value", value);
+
+            form.append(field);
+        });
+
+        // The form needs to be a part of the document in
+        // order for us to be able to submit it.
+        $.ajax({
+            url:'/image',
+            type:'post',
+            data:form.serialize(),
+            success:callback
+        });
+        // $(document.body).append(form);
+        // form.submit();
+        console.log("submitted")
+    }    
+})
--- server.py
+++ server.py
@@ -1,28 +1,68 @@
+from __future__ import print_function
+import base64
+import json
 from imgurpython import ImgurClient
+import threading
+import random
+from random import gauss
+import os
 
 imgur_client = "0f8ebdce6b83981"
 imgur_secret = "f5b5ca46e0ea44e94b5cc3f6e9ebe4dc4a8aa254"
 client = ImgurClient(imgur_client, imgur_secret)
-print(client.upload_from_path("karthik.png"))
+#print(client.upload_from_path("karthik.png"))
 
 
 
 
-from __future__ import print_function
 from flask import Flask, request, send_from_directory
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def root():
-    print("ayyy")
     return send_from_directory('face-demo', 'index.html')
 
-@app.route('/<string:path>')
+@app.route('/<string:path>', methods=['GET'])
 def static_proxy(path):
-    print(path)
     return send_from_directory('face-demo', path)
 
+@app.route('/emojis/<string:path>', methods=['GET'])
+def static_proxy2(path):
+    return send_from_directory('face-demo/emojis', path)
+
+
+counter = 0
+counter_lock = threading.Lock()
+
+@app.route('/image', methods=['POST'])
+def upload_image():
+    #print(request.form['image'])
+    global counter
+    with counter_lock:
+        filename = "images/image%d.jpeg" % counter
+        counter += 1
+    with open(filename, "wb") as fh:
+        fh.write(base64.decodestring(bytes(request.form['image'][23:], 'utf-8')))
+    link = upload_to_imgur(filename)
+    os.remove(filename)
+    emotion = classify_image(link)
+    return emotion
+
+
+def upload_to_imgur(path):
+    link = client.upload_from_path(path)['link']
+    with open("links.txt", "a") as myfile:
+        myfile.write("%s\n" % link)
+    return link
+
+def classify_image(link):
+
+    # vec = [gauss(0, 1) for i in range(8)]
+    # mag = sum(x**2 for x in vec) ** .5
+    # vector = {emotion: score for emotion, score in zip(("anger","contempt","disgust","fear","happiness","neutral","sadness","surprise"), [x/mag for x in vec])}
+    return random.choice(["anger","contempt","disgust","fear","happiness","neutral","sadness","surprise"])
+    #return vector
 
 if __name__ == "__main__":
     app.run(debug=True)
